cc8da12080e2f631a4eb10cbd28a5b6a34e8c7b4
John Patrick Connors <johnpconnors@gmail.com>
2013-08-06 15:30:07 -0400

    adding files

---
+++ RandStratSample.py
@@ -0,0 +1,29 @@
+import os, sys, random, ogr
+from shapely import wkb, geometry
+
+#Define number of random sample for each class
+
+#Define the minimum distance between points
+
+#Define Buffer Size
+
+
+#Define Buffer Shape 
+
+
+#Select each individual category from defining shapefile
+
+
+#Loop through each selection
+
+
+#Create new layer/feature based on this selection
+
+
+#Create random points within the new layer/feature
+
+
+#Create polygons around points
+
+
+#
