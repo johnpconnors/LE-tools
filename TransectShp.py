e97d0b5073bd20ab9f286de460f5bbe397114cc9
John Patrick Connors <johnpconnors@gmail.com>
2013-09-28 16:56:34 -0700

    created function to build buffers

--- TransectShp.py
+++ TransectShp.py
@@ -5,48 +5,86 @@
 import osgeo, os, sys, gdal, math
 from osgeo import ogr
 
-#Set the environment
-def setup(datapath):
+def run(datapath, startx, starty, endx, endy, dist):
 	os.chdir(datapath)
-
-#Define the start and end points of the transect
-#Specify the Buffer
-#Specify the distance
-def transectpoints(shpout, start, end, dist):
-	slope = (endy-starty)/(endx-startx)
-	
-	angle = math.atan(slope)
-
-	xmove = dist * math.cos(angle)
-	ymove = dist * math.sin(angle)
-
+	startx = float(startx)
+	starty = float(starty)
+	endx = float(endx)
+	endy = float(endy)
+	dist = float(dist)
+	print starty
+	move = transectpoints(startx, starty, endx, endy, dist)
+	pts = [startx, starty]
 	#Create a Shapefile
+	spatialReference = osgeo.osr.SpatialReference()
+	spatialReference.ImportFromProj4('+proj=longlat +ellps=WGS84 +datum=WGS84 +no_defs')
 	driver = osgeo.ogr.GetDriverByName('ESRI Shapefile')
 	shapeData = driver.CreateDataSource('transectpoints.shp')
-	
+	outfile = driver.CreateDataSource('buffers.shp')
 	#Create Layer
 	layer = shapeData.CreateLayer('layer1', spatialReference, osgeo.ogr.wkbPoint)
 	layerDef = layer.GetLayerDefn()
+	outlayer = outfile.CreateLayer('layer1', spatialReference, osgeo.ogr.wkbPolygon)
+	outLayerDef = layer.GetLayerDefn()
+	#loop through to calculate new points and save to shapefile
+	totaldist = math.sqrt(((endx-startx)**2)+((endy-starty)**2))
+	print(totaldist)
+	numpts=int(round(totaldist/dist))
+	for i in range(numpts):
+		pts = createpts(pts, move, layer, layerDef)
+	shapeData.Destroy()
+	buffers(outlayer, dist)
 
+def transectpoints(startx, starty, endx, endy, dist):
+	#Define the start and end points of the transect
+	#Specify the Buffer
+	#Specify the distance
+	slope = (endy-starty)/(endx-startx)
+	angle = math.atan(slope)
+	xmove = dist * math.cos(angle)
+	print xmove
+	ymove = dist * math.sin(angle)
+	print ymove
+	move = [xmove, ymove]
+	return move
 
-def creatept():
-	xpt = xpt + xmove
-	ypt = ypt + ymove
-
+def createpts(pts, move, layer, layerDef):
+	print 'go'
+	xpt = pts[0]
+	ypt = pts[1]
+	xpt = xpt + move[0]
+	ypt = ypt + move[1]
+	pts = [xpt, ypt]
+	print pts
 	#Create Feature
 	feat = ogr.Feature(layerDef)
-	feat.SetField ("Name", Name)
-
 	#Create a New Feature for the Shapefile
 	point = ogr.Geometry(ogr.wkbPoint)
 	point.SetPoint(0, xpt, ypt)
+	feat.SetGeometry(point)
+	layer.CreateFeature(feat)
+	print pts
+	return pts
 
-	feat.SetGeometry(pt)
-
-def buffers(buffers):
+def buffers(outlayer, dist):
 #Loop through Features to buffer them
+	ds=ogr.Open('/Users/johnpconnors/testing/transectpoints.shp', 1)
+	print ds
+	lc=ds.GetLayerCount()
+	print lc
+	lyr=ds.GetLayer()
+	fc=lyr.GetFeatureCount()
+	print fc
+	print dist
 
+
+	for i in range(fc):
+		print i
+		feat=lyr.GetFeature(i)
+		geom=feat.GetGeometryRef()
+		feat.SetGeometry(geom.Buffer(dist))
+		outlayer.CreateFeature(feat)
 
 if __name__ == '__main__':
 	#1 is datapath, 2 is shapefile, 3 is transect start, 4 is transect end, 5 is buffer size, 6 is distance between points
-	run(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5])
+	run(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4], sys.argv[5], sys.argv[6])
